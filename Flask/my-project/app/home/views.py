1b0a98004b427c9ec42742009675e0dfdd242109
Clinton Campbell <clintoncampbell@macbook-pro.attlocal.net>
2020-07-07 10:39:28 -0500

    add admin dashboard

--- Flask/my-project/app/home/views.py
+++ Flask/my-project/app/home/views.py
@@ -1,14 +1,14 @@
 # app/home/views.py
 
-from flask import render_template
+from flask import abort, render_template
 from flask_login import current_user, login_required
 
 from . import home
 
 
-@home.route('admin/dashboard')
+@home.route('/admin/dashboard')
 @login_required
-def amdin_dashboard():
+def admin_dashboard():
     # prevent non-admins from accessing the page
     if not current_user.is_admin:
         abort(403)
